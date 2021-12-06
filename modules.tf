0c1b8b2d5edaeb89a38ecf337825cab7d5a7a070
Adam Norris <anorris@rivasolutionsinc.com>
2021-12-06 11:35:47 -0500

    Trying a new reference

--- modules.tf
+++ modules.tf
@@ -49,7 +49,7 @@ module "code-server" {
 }
 
 output "codeserver_password" {
-  value = nonsensitive(module.module_secret.secret)
+  value = nonsensitive(module.code-server.module_secret)
 }
 
 ##########################################
