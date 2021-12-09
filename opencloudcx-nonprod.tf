b9693916d0392d1cfb3f7f161632ca7bdfa2089a
Adam Norris <anorris@rivasolutionsinc.com>
2021-12-08 20:03:24 -0500

    Split modules to dev and nonprod

--- opencloudcx-nonprod.tf
+++ opencloudcx-nonprod.tf
@@ -1,18 +1,18 @@
-module "opencloudcx-aws-dev" {
+module "opencloudcx-aws-nonprod" {
   source = "../module-opencloudcx-aws"
   # source = "git::ssh://git@github.com/OpenCloudCX/module-opencloudcx-aws?ref=develop"
 
-  name             = "opencloudcx-dev-${random_string.scope.result}"
+  name             = "opencloudcx-nonprod-${random_string.scope.result}"
   cluster_version  = var.kubernetes_version
   region           = var.aws_region
   worker_groups    = var.worker_groups
   map_users        = var.eks_map_users
   map_roles        = var.eks_map_roles
   write_kubeconfig = var.write_kubeconfig
-  stack            = "dev"
+  stack            = "nonprod"
 
 
-  dns_zone = "dev.${var.dns_zone}"
+  dns_zone = "nonprod.${var.dns_zone}"
 
   kubernetes_dockerhub_secret_name     = var.kubernetes_dockerhub_secret_name
   kubernetes_secret_dockerhub_username = var.kubernetes_secret_dockerhub_username
