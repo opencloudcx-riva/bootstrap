c7eb56f51edf644070b8d65b971b85614d66c22c
Adam Norris <anorris@rivasolutionsinc.com>
2022-04-01 11:16:47 -0400

    Added output for sonarqube password

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,10 @@ output "grafana_password" {
   value = nonsensitive(module.opencloudcx-aws-mgmt.grafana_secret)
 }
 
+output "sonarqube_password" {
+  value = nonsensitive(module.opencloudcx-aws-mgmt.sonarqube_secret)
+}
+
 # output "sonarqube_key" {
 #   value = module.opencloudcx-aws-mgmt.sonarqube_key
 # }
