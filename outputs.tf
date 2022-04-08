242fe337872de9df4c3740ce1350c29493b26e46
Adam Norris <anorris@rivasolutionsinc.com>
2022-04-08 09:57:40 -0400

    Updated outputs for generic module

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 # Developed by RIVA Solutions Inc 2022.  Authorized Use Only
 
-output "jenkins_password" {
-  value = nonsensitive(module.opencloudcx-aws-mgmt.jenkins_secret)
-}
+# output "jenkins_password" {
+#   value = nonsensitive(module.opencloudcx-aws-mgmt.jenkins_secret)
+# }
 
-output "grafana_password" {
-  value = nonsensitive(module.opencloudcx-aws-mgmt.grafana_secret)
-}
+# output "grafana_password" {
+#   value = nonsensitive(module.opencloudcx-aws-mgmt.grafana_secret)
+# }
 
-output "sonarqube_password" {
-  value = nonsensitive(module.opencloudcx-aws-mgmt.sonarqube_secret)
-}
+# output "sonarqube_password" {
+#   value = nonsensitive(module.opencloudcx-aws-mgmt.sonarqube_secret)
+# }
 
 # output "sonarqube_key" {
 #   value = module.opencloudcx-aws-mgmt.sonarqube_key
